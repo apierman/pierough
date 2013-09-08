0683d3ca13e905f1fe1cd7ff926e4c355e932c47
apierman <apierman@gmail.com>
2013-09-08 11:56:30 -0400

    page load and footer

--- scripts/js/clock.js
+++ scripts/js/clock.js
@@ -1,28 +1,42 @@
-function startTime()
-{
-   var today=new Date();
-   var h=today.getHours();
-   var m=today.getMinutes();
-   var s=today.getSeconds();
-   if (h === 0) h = 12;  
-   if (h > 12) h -= 12;  
-   m = m < 10 ? "0" + m : m;
-   s = s < 10 ? "0" + s : s;
-   return h+":"+m+":"+s;
+function displayTime(sElementId, iFormat) {
+	var sTimeDate;
+	var today=new Date();
+	var d=today.getDate();
+	var mo=today.getMonth() + 1;
+	var y=today.getFullYear();
+	var h=today.getHours();
+	var m=today.getMinutes();
+	var s=today.getSeconds();
+	var merdiem = h >= 12 ? "pm" : "am";
+	if (h === 0) h = 12;  
+	if (h > 12) h -= 12;
+	if (m < 10) m = "0" + m;
+	if (s < 10) s = "0" + s;
+	switch(iFormat)
+	{
+		case 1:
+			sTimeDate=h+":"+m+":"+s+" "+merdiem;
+			break;
+		case 2:
+			sTimeDate=mo+"/"+d+"/"+y+" "+h+":"+m+":"+s+" "+merdiem;
+			break;
+		case 3:
+			sTimeDate=today.toLocaleTimeString();
+			break;
+		case 4:
+			sTimeDate=today.toLocaleString();
+			break;
+		case 5:
+			sTimeDate=today.toDateString() + " " + today.toLocaleTimeString();
+			break;
+	}
+	document.getElementById(sElementId).innerHTML=sTimeDate;
+	   //var t=setTimeout(function(){displayTime(sElementID, iFormat)},1000);
 }
 
-function displayClock() {
-   var h = dt.getHours();
-   var m = dt.getMinutes();
-   var s = dt.getSeconds();
-   var meridiem = h < 12 ? "am" : "pm";
-   if (h === 0) h = 12;  
-   h = h > 12 ? h -= 12 : h;
-   m = m < 10 ? "0" + m : m;
-   s = s < 10 ? "0" + s : s;
-   var str =  h + ":" + m + ":" + s + merdiem;
-//   document.write(str); 
-//   document.getElementById("showClock").innerHTML=str;
-//   setTimeout(function { displayClock(); }, 2000);
-   return str;
+function runClock(sElementId, iFormat)
+{
+	if (sElementId === undefined) sElementId = "showClock";
+	if (iFormat === undefined) iFormat = 3;
+	var varClock=setInterval(function() {displayTime(sElementId, iFormat)}, 1000);
 }
