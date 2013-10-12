6e9a709ef931d76e98c1b0b26e0b19a07518989e
apierman <apierman@gmail.com>
2013-10-12 09:23:24 -0400

    guess-number comments
    added comments to guess number

--- scripts/js/guessnum.js
+++ scripts/js/guessnum.js
@@ -1,3 +1,7 @@
+
+// Create an object to hold the random number and the 
+// guesses and min and max.  Also a game message and
+// if finished variable.
 function RandNumber(max, guesses) {
 	this.number = Math.round(Math.random() * max);
 	this.guess = -1;
@@ -9,7 +13,7 @@ function RandNumber(max, guesses) {
 	this.finished = false;
 }
 
-	// Get input from the user
+	// Add RandNumber function to get input from the user
 	RandNumber.prototype.getGuess = function () { 
 		this.guess = parseInt(prompt(this.message + "[" + this.guesses + "/" + this.maxGuesses + "] " +
 			"Enter a number between " + this.min + " and " + this.max));
@@ -17,13 +21,13 @@ function RandNumber(max, guesses) {
 			this.finished = true;
 			return 0;
 		} 
-		if (this.guess > this.max || this.guess < this.min) {
+		if (this.guess >= this.max || this.guess <= this.min) {
 			this.message = "Invalid Guess: " + this.guess + "\n";
 			this.getGuess();
 		}
 	};
 
-	// Check the validity of the guess 
+	// Add RandNumber function to check the validity of the guess 
 	RandNumber.prototype.checkGuess = function () { 
 		// check if found number
 		if (this.number == this.guess) {
@@ -47,6 +51,7 @@ function RandNumber(max, guesses) {
 		}		
 	};
 
+// Create an instance of RandNumber and start the guessing.
 function playGuess (max, guesses) 
 {
     var numbers = new RandNumber(max, guesses);
